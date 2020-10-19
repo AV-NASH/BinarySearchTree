a477adcf7e920fd131e073b45b06fb555c1d771e
AV-NASH <avinash21213434@gmail.com>
2020-10-20 00:02:55 +0530

    Added add and size method in binary search tree with for 3 values with test case

--- src/main/java/com/cg/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/cg/binarysearchtree/BinarySearchTree.java
@@ -1,4 +1,28 @@
 package com.cg.binarysearchtree;
 
-public class BinarySearchTree {
+public class BinarySearchTree<E extends Comparable> {
+    private  Node<E> rootNode;
+
+    public void add(E data){
+        Node addnode=new Node(data);
+        if(rootNode==null) rootNode=addnode;
+        else {
+            if(rootNode.data.compareTo(data)>0) rootNode.left=addnode;
+            else rootNode.right=addnode;
+        }
+    }
+
+
+
+    public int size(){
+        int size =0;
+        if(rootNode==null) return size;
+        else {size++;
+            if(rootNode.left!=null) size++;
+            if(rootNode.right!=null) size++;
+            return size;
+        }
+    }
+
+
 }
--- src/test/java/com/cg/binarysearchtree/BinarySearchTreeTest.java
+++ src/test/java/com/cg/binarysearchtree/BinarySearchTreeTest.java
@@ -1,7 +1,18 @@
 package com.cg.binarysearchtree;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class BinarySearchTreeTest {
 
+    @Test
+    public void whenDataEntered_inBinaryTree_returnsCorrectSize() {
+        BinarySearchTree binarySearchTree=new BinarySearchTree();
+        binarySearchTree.add(56);
+        binarySearchTree.add(30);
+        binarySearchTree.add(70);
+        Assert.assertEquals(3,binarySearchTree.size());
+    }
 }
